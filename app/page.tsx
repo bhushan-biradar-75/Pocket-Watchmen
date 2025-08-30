773661672f73b0785dabf8ef269d0c9ca8840bb2
bhushan-biradar-75 <bhushanbiradar75@gmail.com>
2025-08-30 16:21:29 +0530

    Added middle ware & clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-  <div>
-    <Card/>
-    <h1>working</h1>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+       <h1 className="text-center border-8">Bhola</h1>
+       <Button className="border-8 p-4 bg-background:red">Submit bappa</Button>
+       <Card/>
+          </main>       
     </div>
-
   );
 }
